21fc1126b34998e69b3e3b237e54e86d9013c3ab
RexShack <rexshackuk@gmail.com>
2022-10-30 10:36:15 +0000

    Initial commit

---
+++ installation/shared_items.lua
@@ -0,0 +1,8 @@
+	-- items
+	['shovel']	= {['name'] = 'shovel',	['label'] = 'Shovel',	['weight'] = 100,	['type'] = 'item',	['image'] = 'shovel.png',	['unique'] = false,	['useable'] = true,	['shouldClose'] = true,	['combinable'] = nil,	['level'] = 0,	['description'] = 'equipment for digging'},
+
+	-- metal detector weapon (shared items)
+	['weapon_kit_metal_detector']	= {['name'] = 'weapon_kit_metal_detector',	['attachPoint'] = 11,	['label'] = 'Metal Detector',	['weight'] = 100,	['type'] = 'weapon',	['ammotype'] = nil,	['image'] = 'weapon_kit_metal_detector.png',	['unique'] = true,	['useable'] = true,	['level'] = 0,	['description'] = 'metal detector'},
+
+	-- metal detector weapon (shared weapons)
+	[`weapon_kit_metal_detector`]	= {['name'] = 'weapon_kit_metal_detector',	['attachPoint'] = 11,	['label'] = 'Metal Detector',	['weight'] = 100,	['type'] = 'weapon',	['ammotype'] = nil,	['image'] = 'weapon_kit_metal_detector.png',	['unique'] = true,	['useable'] = true,	['level'] = 0,	['description'] = 'metal detector'},
